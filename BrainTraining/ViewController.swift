79212e3c2aa8cc7d2f264b45dfac2da81fa264cf
Ljubomir Masirevic <ljubomirmasirevic@ljubomirmacbook.local>
2018-07-19 18:46:38 +0200

    Initial Commit

---
+++ BrainTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainTraining
+//
+//  Created by Ljubomir Masirevic on 7/19/18.
+//  Copyright © 2018 Ljubomir Masirevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
